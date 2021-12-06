3900917c95b67be7b51baf86b42fca983ee179dc
Guido Faecke <gfaecke@perforce.com>
2021-12-06 16:00:23 -0700

    Initial commit

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,8 +2,12 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
+        <v-row justify="center" class="ma-5" style="font-size: 3rem">
+          <h1>Guido Faecke</h1>
+        </v-row>
         <v-row justify="center" class="ma-5">
-          <span>About me</span>
+          <p class="">Hi, my name is Guido Faecke and I am a PHP Professional Service Consultant for <a href="https://www.perforce.com">Perforce</a>.</p>
+          <p class="">On my <a href="https://github.com/guidofaecke">GitHub</a> account, I provide tips &amp; tricks for IBM i, as well as some solutions for Doctrine DBAL in regards to the IBM i.</p>
         </v-row>
         <v-row justify="center" class="ma-5">
           <v-btn @click="changeComponent('MyGithub')">
