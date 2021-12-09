c741223b5b6f56bad9bfcc4a21deefb6a88411ad
Guido Faecke <gfaecke@perforce.com>
2021-12-09 10:09:16 -0700

    Adding some sparkles

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,16 +1,26 @@
 /* eslint-disable no-console */
-const execa = require("execa");
+// import {execa} from 'execa';
+const execa = require('execa');
 const fs = require("fs");
+
 (async () => {
     try {
         console.log("Building started...");
         await execa("npm", ["run", "build"]);
         // Understand if it's dist or build folder
         const folderName = fs.existsSync("dist") ? "dist" : "build";
+        console.log("Adding " + folderName + "to repository...")
         await execa("git", ["add", "-f", folderName]);
+        console.log("Committing gh-pages...")
         await execa("git", ["commit", "-m", "gh-pages"]);
         console.log("Pushing to gh-pages...");
         await execa("git", ["subtree", "push", "--prefix", folderName, "origin", "gh-pages"]);
+        console.log("Removing " + folderName + "from repository...");
+        await execa("git", ["rm", "-r", folderName]);
+        console.log("Committing without dist...")
+        await execa("git", ["commit", "-m", "remove_dist"]);
+        console.log("Pushing repository...")
+        await execa("git", ["push", "origin", "main"]);
         console.log("Successfully deployed, check your settings");
     } catch (e) {
         // eslint-disable-next-line no-console
--- src/router/routes.js
+++ src/router/routes.js
@@ -23,19 +23,21 @@ export default [
         }
     },
     {
-        path: 'https://github.com/guidofaecke',
+        path: '/github',
+        beforeEnter() {location.href = 'https://github.com/guidofaecke'},
         name: 'Github',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
         meta: {
-            title: "Gitub",
+            title: "Github",
             icon: "mdi-github"
         }
     },
     {
-        path: 'https://twitter.com/phpdudeaz',
+        path: '/twitter',
+        beforeEnter() {location.href = 'https://twitter.com/phpdudeaz'},
         name: 'Twitter',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
@@ -47,7 +49,8 @@ export default [
         }
     },
     {
-        path: 'https://www.linkedin.com/in/guido-faecke/',
+        path: '/linkedin',
+        beforeEnter() {location.href = 'https://www.linkedin.com/in/guido-faecke/'},
         name: 'LinkedIn',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
